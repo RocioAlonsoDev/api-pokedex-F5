1f6c83ad766c58c68927d250802f8ab381bfe843
Rocio <rocioalonso.dev@gmail.com>
2023-06-30 10:49:00 +0200

    api pokedex finished

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const pokemon_count = 151
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -16,3 +16,35 @@ const colors = {
 	fighting: '#E6E0D4',
 	normal: '#F5F5F5'
 }
+
+const APIurl = 'https://pokeapi.co/api/v2/pokemon/'
+
+const getPokemon = async() => {
+	try{
+		for(i=1;i<=pokemon_count;i++){
+			const {data} = await axios.get(APIurl+i)
+		
+			
+			poke_container.innerHTML += 
+			`<div class="pokemon" style="background-color: ${colors[data.types[0].type.name]}">
+				<div class="img-container">
+			  		<img src="${data.sprites.front_default}" id="sprite" alt="${data.name}">
+				</div>
+				<div class="info">
+			  		<span class="number" id="number">#${data.id}</span>
+			  		<h3 class="name" id="name">${data.name}</h3>
+			  		<small class="type" id="type">Type: <span>${data.types[0].type.name}</span></small>
+				</div>
+			</div>`;
+			
+		}
+	}catch(error){
+		console.log(error)
+	}
+}
+
+const createPokemonCard = () => {
+
+}
+
+getPokemon()
